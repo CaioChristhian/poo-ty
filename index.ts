08350ef29e5430b33c08f4da1bdb8b4b6f37a5b5
CaioChristhian <caiochristhianz@gmail.com>
2025-05-07 17:00:34 -0300

    getter setters

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { Person } from "./banking/Person";
 function main() {
   const maria = new Person("Maria", "da Silva", new Date("1999-05-11"));
 
-  console.log(maria.getFullName());
+  console.log(maria.fullName);
 }
 
 main();
